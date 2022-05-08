48c9e2cbf79970fa72e64844c6e1dbd86b43cb38
On3s228 <77531256+On3s228@users.noreply.github.com>
2022-05-08 21:25:21 +0300

    a

--- src/Service/SocialsUserService.php
+++ src/Service/SocialsUserService.php
@@ -5,22 +5,15 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
-use KnpU\OAuth2ClientBundle\Client\OAuth2ClientInterface;
+use DateTime;
 use League\OAuth2\Client\Provider\FacebookUser;
 use League\OAuth2\Client\Provider\GoogleUser;
-use League\OAuth2\Client\Token\AccessToken;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
 class SocialsUserService
 {
     private function getUserBase() : ?User{
         return (new User())
-            ->setRegisterDate(new \DateTime())
-            ->setLikes(0)
+            ->setRegisterDate(new DateTime())
             ->setPassword('0');
     }
 
